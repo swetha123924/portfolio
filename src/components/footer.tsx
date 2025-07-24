612bc4baeecba20cfb4a0cb87cc3f3f85febb094
SK-Saba-Tarannum <sabatarannum0303@gmail.com>
2025-07-24 07:23:22 +0530

    small changes in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC= () => {
   return (
     <footer className='bg-gray-800 flex-col md:flex gap-4 justify-center text-white p-4 text-center'>
       <p className='font-bold text-blue-500'>Contact:</p>
-      <span>swethasree630@gmail.com</span>
+      <span>swethasree6300@gmail.com</span>
       <div className='flex justify-center space-x-4'>
         <a href="#" className='text-orange-500 hover:text-orange-300'>LinkedIn</a>
         <a href="https://github.com/swetha123924" className='text-orange-500 hover:text-orange-300'>GitHub</a>
